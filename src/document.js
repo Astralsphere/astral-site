94784d6ff68a9c45b94f5c5ab2b10cb8fb40d9a8
PlopesK <gp081009@gmail.com>
2024-11-25 23:11:36 -0300

    Adicionada mudança automática de imagens

--- src/document.js
+++ src/document.js
@@ -7,3 +7,25 @@ $(document).ready(function () {
     $(this).addClass("animated"); 
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.social img').forEach(function(img) {
+    const originalSrc = img.src;
+
+    img.addEventListener('mouseover', function() {
+        img.src = originalSrc.replace('.png', ' hover.png');
+    });
+
+    img.addEventListener('mouseout', function() {
+        img.src = originalSrc;
+    });
+
+    img.addEventListener('mousedown', function() {
+        img.src = originalSrc.replace('.png', ' pressed.png');
+    });
+
+    img.addEventListener('mouseup', function() {
+        img.src = originalSrc.replace('.png', ' hover.png');
+    });
+  });
+});
