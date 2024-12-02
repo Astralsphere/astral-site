ddcbc0085da2847aec2ba30216b0ad614d2e3f16
PlopesK <gp081009@gmail.com>
2024-12-01 21:44:22 -0300

    Arrumando uns problemas que apareceram

--- src/document.js
+++ src/document.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $("a").on("animationiteration", function () {
     $(this).removeClass("animated");
   });
-
   $("a").hover(function () {
     $(this).addClass("animated"); 
   });
@@ -12,7 +11,6 @@ $(document).ready(function () {
 function changeFlag() {
   var select = document.getElementById("ddlLanguage");
   var img = document.getElementById("languageFlag");
-  
   if (select.value === "en") {
       img.src = "imgs/Dictionary/usa.png";
   } else if (select.value === "pt-br") {
@@ -24,19 +22,15 @@ function changeFlag() {
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.social img').forEach(function(img) {
     const originalSrc = img.src;
-
     img.addEventListener('mouseover', function() {
         img.src = originalSrc.replace('.png', ' hover.png');
     });
-
     img.addEventListener('mouseout', function() {
         img.src = originalSrc;
     });
-
     img.addEventListener('mousedown', function() {
         img.src = originalSrc.replace('.png', ' pressed.png');
     });
-
     img.addEventListener('mouseup', function() {
         img.src = originalSrc.replace('.png', ' hover.png');
     });
@@ -44,12 +38,10 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /* Trailer começa pausado */
-
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('youtube-video', {
@@ -58,7 +50,6 @@ function onYouTubeIframeAPIReady() {
         }
     });
 }
-
 function onPlayerReady(event) {
     event.target.pauseVideo();
 }
